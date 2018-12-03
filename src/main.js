0f9a4bff3d70fef08faf63273c514516be939a4b
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-12-02 18:19:33 -0800

    Add remaining 2 game functions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 const colorButton = document.querySelectorAll('#colorGrid section button');
+const numberButton1 = document.querySelectorAll('#colorGrid2 section button');
+const numberButton2 = document.querySelectorAll('#colorGrid3 section button');
+
 const colorSection = document.querySelector('#colorGrid');
+const numberSection1 = document.querySelector('#colorGrid2');
+const numberSection2 = document.querySelector('#colorGrid3');
 
 function wrapper() {
 
-  const changeColor = () => {
+  const transition1 = () => {
     colorSection.classList.toggle('colorChange');
   };
 
@@ -13,15 +18,94 @@ function wrapper() {
 
     for (let i = 0; i < thisButtonLength * 2; i++) {
       setTimeout(function() { 
-        changeColor();
+        transition1();
       }, i * 1000);
     }
+
+    setTimeout(function() {
+      colorSection.classList.add('hide');
+      numberSection1.classList.remove('hide');
+      numberSection1.classList.add('show');
+    },thisButtonLength * 2000);
 
   };
   loopThrough();
 };
 
+
+
+function wrapper2() {
+
+  const transition2 = () => {
+    numberSection1.classList.toggle('colorChange');
+  };
+
+  const loopThrough2 = () => {
+    let thisNumber = this.childNodes[0].nodeValue;
+    //nodeValue will be returned as a string, so we need to interpret it as a number value
+    let numberValue = Number(thisNumber);
+
+    for (let i = 0; i < numberValue * 2; i++) {
+      setTimeout(function() { 
+        transition2();
+      }, i * 1000);
+    }
+
+    setTimeout(function() {
+      numberSection1.classList.add('hide');
+      numberSection1.classList.remove('show');
+      numberSection2.classList.remove('hide');
+      numberSection2.classList.add('show');
+    },numberValue * 2000);
+
+  };
+  loopThrough2();
+};
+
+
+
+function wrapper3() {
+
+  const transition3 = () => {
+    numberSection2.classList.toggle('colorChange');
+  };
+
+  const loopThrough3 = () => {
+    let thisNumber = this.childNodes[0].nodeValue;
+    //nodeValue will be returned as a string, so we need to interpret it as a number value
+    let numberValue = Number(thisNumber);
+
+    for (let i = 0; i < numberValue * 2; i++) {
+      setTimeout(function() { 
+        transition3();
+      }, i * 1000);
+    }
+
+    setTimeout(function() {
+      numberSection2.classList.add('hide');
+      numberSection2.classList.remove('show');
+      /*
+      //then tell fortune to appear here!
+      fortune.Container.classList.add('class-name-here')
+      */
+    },numberValue * 2000);
+
+  };
+  loopThrough3();
+};
+
+
+
+//event 1
 for(let i = 0; i < colorButton.length; i++) {
   colorButton[i].addEventListener('click', wrapper);
+};
+//event 2
+for(let i = 0; i < numberButton1.length; i++) {
+  numberButton1[i].addEventListener('click', wrapper2);
+};
+//event 3
+for(let i = 0; i < numberButton2.length; i++) {
+  numberButton2[i].addEventListener('click', wrapper3);
 };
 
